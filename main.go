c1e81b85d447b641f72fbac2b54fa30671b3b3c0
devendr4 <angel.emo.altuve@gmail.com>
2021-03-13 12:09:31 -0400

    port ENV

--- main.go
+++ main.go
@@ -78,6 +78,6 @@ func main() {
 
 	handler := cors.Default().Handler(router)
 
-	fmt.Println("Listening on port 8080...")
+	fmt.Println("Listening on port" + os.Getenv("PORT") + "...")
 	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), handler))
 }
